33e0bff3df7c592f9400da28046bd047519340ee
SelmanDemiray <selmandemiray@outlook.com>
2026-03-09 13:04:43 -0400

    added new runbook updated

--- runbooks/graphfix.ps1
+++ runbooks/graphfix.ps1
@@ -1,11 +1,20 @@
-https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.Authentication/2.25.0
+https://www.powershellgallery.com/api/v2/package/Az.Accounts
+https://www.powershellgallery.com/api/v2/package/Az.Storage
+https://www.powershellgallery.com/api/v2/package/Az.ResourceGraph
+https://www.powershellgallery.com/api/v2/package/Az.Security
 
-https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.Identity.DirectoryManagement/2.25.0
 
-https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.Users/2.25.0
 
-https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.Groups/2.25.0
 
-https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.Applications/2.25.0
+https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.Authentication
+https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.Identity.DirectoryManagement
+https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.Users
+https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.Groups
+https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.Applications
+https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.DeviceManagement
 
-https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.DeviceManagement/2.25.0
+
+
+
+https://www.powershellgallery.com/api/v2/package/PSFramework
+https://www.powershellgallery.com/api/v2/package/ZeroTrustAssessment
