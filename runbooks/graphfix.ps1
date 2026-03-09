a6ed6a574325182d7d5e95861e710cadf17c9e99
SelmanDemiray <selmandemiray@outlook.com>
2026-03-09 13:08:46 -0400

    added new runbook updated

--- runbooks/graphfix.ps1
+++ runbooks/graphfix.ps1
@@ -18,3 +18,30 @@ https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.DeviceManagemen
 
 https://www.powershellgallery.com/api/v2/package/PSFramework
 https://www.powershellgallery.com/api/v2/package/ZeroTrustAssessment
+
+
+
+
+
+$aa = "YourAutomationAccountName"
+$rg = "YourResourceGroupName"
+$rt = "YourPS74RuntimeEnvironmentName"
+
+$modules = @(
+    "Az.Accounts", "Az.Storage", "Az.ResourceGraph", "Az.Security",
+    "Microsoft.Graph.Authentication", "Microsoft.Graph.Identity.DirectoryManagement",
+    "Microsoft.Graph.Users", "Microsoft.Graph.Groups", "Microsoft.Graph.Applications",
+    "Microsoft.Graph.DeviceManagement", "PSFramework", "ZeroTrustAssessment"
+)
+
+foreach ($mod in $modules) {
+    Write-Host "Importing $mod..." -ForegroundColor Cyan
+    New-AzAutomationModule `
+        -AutomationAccountName $aa `
+        -ResourceGroupName $rg `
+        -Name $mod `
+        -ContentLinkUri "https://www.powershellgallery.com/api/v2/package/$mod" `
+        -RuntimeVersion '7.4'
+    Start-Sleep -Seconds 5
+}
+Write-Host "All submitted. Monitor status in portal." -ForegroundColor Green
