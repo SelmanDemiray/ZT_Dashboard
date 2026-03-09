c2bd22fbe2f512fb6d8748e179a82494d623d96c
SelmanDemiray <selmandemiray@outlook.com>
2026-03-09 13:12:28 -0400

    added new runbook updated

--- runbooks/graphfix.ps1
+++ runbooks/graphfix.ps1
@@ -23,25 +23,50 @@ https://www.powershellgallery.com/api/v2/package/ZeroTrustAssessment
 
 
 
-$aa = "YourAutomationAccountName"
-$rg = "YourResourceGroupName"
-$rt = "YourPS74RuntimeEnvironmentName"
+$subscriptionId = "YOUR_SUBSCRIPTION_ID"
+$rg             = "YOUR_RESOURCE_GROUP"
+$aa             = "YOUR_AUTOMATION_ACCOUNT"
+$runtimeName    = "YOUR_74_RUNTIME_ENV_NAME"  # exact name you created in portal
+$apiVersion     = "2023-05-15-preview"
+
+# Get token context
+Connect-AzAccount  # skip if already connected in Cloud Shell
 
 $modules = @(
-    "Az.Accounts", "Az.Storage", "Az.ResourceGraph", "Az.Security",
-    "Microsoft.Graph.Authentication", "Microsoft.Graph.Identity.DirectoryManagement",
-    "Microsoft.Graph.Users", "Microsoft.Graph.Groups", "Microsoft.Graph.Applications",
-    "Microsoft.Graph.DeviceManagement", "PSFramework", "ZeroTrustAssessment"
+    "Az.Accounts",
+    "Az.Storage",
+    "Az.ResourceGraph",
+    "Az.Security",
+    "Microsoft.Graph.Authentication",
+    "Microsoft.Graph.Identity.DirectoryManagement",
+    "Microsoft.Graph.Users",
+    "Microsoft.Graph.Groups",
+    "Microsoft.Graph.Applications",
+    "Microsoft.Graph.DeviceManagement",
+    "PSFramework",
+    "ZeroTrustAssessment"
 )
 
 foreach ($mod in $modules) {
-    Write-Host "Importing $mod..." -ForegroundColor Cyan
-    New-AzAutomationModule `
-        -AutomationAccountName $aa `
-        -ResourceGroupName $rg `
-        -Name $mod `
-        -ContentLinkUri "https://www.powershellgallery.com/api/v2/package/$mod" `
-        -RuntimeVersion '7.4'
-    Start-Sleep -Seconds 5
+    $uri  = "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$rg" +
+            "/providers/Microsoft.Automation/automationAccounts/$aa" +
+            "/runtimeEnvironments/$runtimeName/packages/$mod`?api-version=$apiVersion"
+
+    $body = @{
+        properties = @{
+            contentLink = @{
+                uri = "https://www.powershellgallery.com/api/v2/package/$mod"
+            }
+        }
+    } | ConvertTo-Json -Depth 5
+
+    $response = Invoke-AzRestMethod -Uri $uri -Method PUT -Payload $body
+
+    if ($response.StatusCode -in 200, 201, 202) {
+        Write-Host "[$mod] Submitted OK ($($response.StatusCode))" -ForegroundColor Green
+    } else {
+        Write-Host "[$mod] FAILED - $($response.StatusCode): $($response.Content)" -ForegroundColor Red
+    }
+
+    Start-Sleep -Seconds 3  # stagger to avoid throttling
 }
-Write-Host "All submitted. Monitor status in portal." -ForegroundColor Green
